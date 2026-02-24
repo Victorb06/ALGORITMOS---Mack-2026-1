fc7cd53cab7dc640dca6d720a19d39c4c61edb00
Victorb06 <victorbirello@hotmail.com>
2026-02-24 12:40:13 +0000

    update

--- Lab1/exercicio6.c
+++ Lab1/exercicio6.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
 int main(){
-    
+    int inteiro;
+    printf("Digite um valor inteiro:");
+    scanf("%d",&inteiro);
+    if (inteiro % 5 == 0){
+        printf("O número é múltiplo de 5\n");
+    } else {
+        printf("O número não é múltiplo de 5\n");
+    }
+    return 0;
 }
