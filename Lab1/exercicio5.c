7e546b7559d71a82b1a3c8f950cd0ae5e3685b46
Victorb06 <victorbirello@hotmail.com>
2026-02-24 01:11:35 +0000

    update

--- Lab1/exercicio5.c
+++ Lab1/exercicio5.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
 int main(){
-    
+    float salariomin,dolar;
+    printf("Digite o valor do salario mínimo:");
+    scanf("%f",&salariomin);
+    printf("Digite o valor do dolar atualmente:");
+    scanf("%f",&dolar);
+    float totalreal = 10000000 * dolar;
+    float casa = 150 * salariomin;
+    int quantidade = totalreal/casa;
+    printf("A quantidade de casas que podem ser construidas é de: %d\n",quantidade);
+    return 0;
 }
