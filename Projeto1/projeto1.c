39bd25195cc4d20accc2b7ad47bc2c5dc2db3eb3
Victorb06 <victorbirello@hotmail.com>
2026-03-16 20:20:42 +0000

    update

--- Projeto1/projeto1.c
+++ Projeto1/projeto1.c
@@ -2,13 +2,14 @@
 Tradutor de Código Morse
 Projeto 1- Laboratório de Algoritmos e Programação II
 Integrantes:
-
-RA:
-
-RA:
+Enzo Yuri Domingues Ma
+RA:10738664
+Willian Lima de Oliveira Pena
+RA:10428678
 Victor Esteves Gallo Birello
 RA: 10737139
  */
+
 #include <stdio.h>
 #include <string.h>
 
