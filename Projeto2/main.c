8271574bdb6e6c58514610d5e7e9e796259b17ab
Victorb06 <victorbirello@hotmail.com>
2026-04-29 23:43:58 +0000

    update

---
+++ Projeto2/main.c
@@ -0,0 +1,13 @@
+/**
+Sistema de Análise de Dados de Vendas
+Projeto 2- Laboratório de Algoritmos e Programação II
+Integrantes:
+Enzo Yuri Domingues Ma
+RA:10738664
+Victor Esteves Gallo Birello
+RA: 10737139
+Willian Lima de Oliveira Pena
+RA:10428678
+ */
+#include <stdio.h>
+#include <stdlib.h>
