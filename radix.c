82b306568ca4da8ad4509c3e702b14e3d3a74e3d
Victorb06 <victorbirello@hotmail.com>
2026-05-19 16:10:05 +0000

    radix update

--- radix.c
+++ radix.c
@@ -1,61 +1,67 @@
 #include <stdio.h>
 
-int getmax(int v[],int n){
+int getmax(int v[], int n){
     int max = v[0];
 
     for(int i = 1; i < n; i++){
         if(v[i] > max)
-           max = v[i];
+            max = v[i];
     }
     return max;
 }
 
-void countingsort(int v[],int n,int exp){
-    int output[n];
-    int count[10] = {0};
+void bubblesort(int v[], int n, int exp){
 
-    for(int i = 0; i < n; i++){
-        count[(v[i] / exp) % 10]++;
-    }
+    for(int i = 0; i < n - 1; i++){
 
-    for(int i = 1; i < 10; i++){
-        count[i] += count[i - 1];
-    }
+        for(int j = 0; j < n - 1 - i; j++){
 
-    for(int i = n - 1; i >= 0; i--){
-        int index = (v[i] / exp) % 10;
-        output[count[index] - 1] = v[i];
-        count[index]--;
-    }
+            int digito1 = (v[j] / exp) % 10;
+            int digito2 = (v[j + 1] / exp) % 10;
 
-    for(int i = 0; i < n; i++){
-        v[i] = output[i];
+            if(digito1 > digito2){
+
+                int temp = v[j];
+                v[j] = v[j + 1];
+                v[j + 1] = temp;
+            }
+        }
     }
 }
 
-void radixsort(int v[],int n){
-    int max = getmax(v,n);
+void radixsort(int v[], int n){
+
+    int max = getmax(v, n);
+
     for(int exp = 1; max / exp > 0; exp *= 10){
-        countingsort(v,n,exp);
+
+        bubblesort(v, n, exp);
     }
 }
 
 int main(){
-    int v[]={329,457,657,839,436,720,355};
+
+    int v[] = {329, 457, 657, 839, 436, 720, 355};
+
     int n = sizeof(v) / sizeof(v[0]);
 
     printf("Vetor Original:\n");
+
     for(int i = 0; i < n; i++){
-        printf("%d ",v[i]);
+        printf("%d ", v[i]);
     }
+
     printf("\n");
 
-    radixsort(v,n);
+    radixsort(v, n);
 
     printf("Vetor Ordenado:\n");
+
     for(int i = 0; i < n; i++){
-        printf("%d ",v[i]);
+        printf("%d ", v[i]);
     }
+
     printf("\n");
+
     return 0;
 }
